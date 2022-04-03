017aeff31be699ec4cb29dba2df85745892e62eb
AGordey <gordey.andrej@gmail.com>
2022-04-03 23:43:31 +0300

    Fill First name+last name+email+without advertisement

--- src/test/java/com/demoqa/TestingFormTests.java
+++ src/test/java/com/demoqa/TestingFormTests.java
@@ -1,4 +1,29 @@
 package com.demoqa;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
 public class TestingFormTests {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+//        executeJavaScript("$('footer').remove()");
+//        executeJavaScript("$('#fixedban').remove()");
+    }
+
+    @Test
+    void fillFormTest () {
+        open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+        $("[id=firstName]").setValue("OurFirstName");
+        $("[id=lastName]").setValue("OurLastName");
+        $("[id=userEmail]").setValue("OurEmail@email.com");
+    }
 }
