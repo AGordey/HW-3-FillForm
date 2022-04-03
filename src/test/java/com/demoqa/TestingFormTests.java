a0d0c66cc0c2299faa782dfde7a4fceb6e5b8d79
AGordey <gordey.andrej@gmail.com>
2022-04-04 01:16:15 +0300

    Fill text pole without Birthday, hobbie, subject, state, sity

--- src/test/java/com/demoqa/TestingFormTests.java
+++ src/test/java/com/demoqa/TestingFormTests.java
@@ -4,7 +4,11 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class TestingFormTests {
 
@@ -13,8 +17,6 @@ public class TestingFormTests {
         Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-//        executeJavaScript("$('footer').remove()");
-//        executeJavaScript("$('#fixedban').remove()");
     }
 
     @Test
@@ -22,8 +24,28 @@ public class TestingFormTests {
         open("/automation-practice-form");
         executeJavaScript("$('footer').remove()");
         executeJavaScript("$('#fixedban').remove()");
-        $("[id=firstName]").setValue("OurFirstName");
-        $("[id=lastName]").setValue("OurLastName");
-        $("[id=userEmail]").setValue("OurEmail@email.com");
+        //Все простые поля для заполнения
+        $("#firstName").setValue("OurFirstName");
+        $("#lastName").setValue("OurLastName");
+        $("#userEmail").setValue("OurEmail@email.com");
+        $("#userNumber").setValue("1234567890");
+        $("#currentAddress").setValue("OurCurrentAddress");
+        //Поле с радиобаттоном
+        $(".custom-control-label").click(); //надо переделать Выбор гендера
+        //Кнопка Submit
+        $("#submit").click();
+
+        //Проверка введенных данных
+        $(".modal-body").shouldHave
+                (text("OurFirstName"),
+                text("OurLastName"),
+                text("OurEmail@email.com"),
+                text("1234567890"),
+                text("OurCurrentAddress"));
+//        нету проверки гендера
+//        выбор даты рождения
+//                выбор сабджектов
+//                        выбор хобби
+//                                выбор штата и города
     }
 }
