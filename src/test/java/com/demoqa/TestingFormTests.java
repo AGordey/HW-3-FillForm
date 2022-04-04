4113bf3a7ae8ca3b3e1fd91ae063eb10af409a02
AGordey <gordey.andrej@gmail.com>
2022-04-05 00:46:05 +0300

    All fill form

--- src/test/java/com/demoqa/TestingFormTests.java
+++ src/test/java/com/demoqa/TestingFormTests.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.files.DownloadActions.click;
 
 public class TestingFormTests {
 
@@ -31,26 +31,39 @@ public class TestingFormTests {
         $("#userEmail").setValue("OurEmail@email.com");
         $("#userNumber").setValue("1234567890");
         $("#currentAddress").setValue("OurCurrentAddress");
+
         //Поле с радиобаттоном - выбор пола
         $x("//*[@id='genterWrapper']/div[2]/div[2]").click();
+
         //Поле предметов - выпадающее меню
         $("#subjectsInput").setValue("Hindi").pressEnter();
         $("#subjectsInput").setValue("English").pressEnter();
+
         //Выбор хобби
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#hobbiesWrapper").$(byText("Reading")).click();
         $("#hobbiesWrapper").$(byText("Music")).click();
+
         //Выбор города
         $("#stateCity-wrapper").click();
         $(byText("Haryana")).click();
         $("#city").click();
         $(byText("Karnal")).click();
 
+        //Зaгрузка файла
+        $("#uploadPicture").uploadFromClasspath("1.png");
+
+        //Дата рождения
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").$(byText("2000")).click();
+        $(".react-datepicker__month-select").$(byText("January")).click();
+        $("[aria-label$='" + "January" + " " + 9 + "th, " + 2000 + "']").click();
 
         //Кнопка Submit
         $("#submit").click();
+
         //Проверка введенных данных
-        $(".modal-body").shouldHave
+                $(".modal-body").shouldHave
                 (text("OurFirstName"),
                 text("OurLastName"),
                 text("OurEmail@email.com"),
@@ -63,9 +76,9 @@ public class TestingFormTests {
                 text("Music"),
                 text("Haryana"),
                 text("Karnal"),
+                text("1.png"),
+                text("09 January,2000"),
                 text("OurCurrentAddress"));
 
-//      выбор даты рождения
-//      выбор штата и города
     }
 }
